601f9e2a83cdc5d9e3eb44e997de4a647d1286d6
vgalitsky <concept.galitsky@gmail.com>
2025-03-13 11:24:49 -0400

    init

---
+++ src/Api.php
@@ -0,0 +1,48 @@
+<?php
+namespace Exrray;
+
+class Api
+{
+    public static function merge(array &$array1, array &$array2): array
+    {
+        $merged = &$array1;
+
+        foreach ($array2 as $key => $value) {
+            if (is_array($value) && array_key_exists($key, $merged) && is_array($merged[$key])) {
+                $merged[$key] = static::merge($merged[$key], $value);
+            } else {
+                $merged[$key] = $value;
+            }
+        }
+
+        return $merged;
+    }
+
+    public static function walk(array &$array, callable $callback): void
+    {
+        foreach ($array as $key => &$value) {
+            if (is_array($value)) {
+                static::walk($value, $callback);
+            } else {
+                $callback($value, $key);
+            }
+        }
+    }
+
+    // public static function reduce(array &$array, callable $callback, mixed $initial = null): mixed
+    // {
+    //     $accumulator = $initial;
+
+    //     foreach ($array as $key => &$value) {
+    //         if (is_array($value)) {
+    //             $value = static::reduce($value, $callback, $initial);
+    //         }
+
+    //         $accumulator = $callback($accumulator, $value, $key);
+
+    //         unset($array[$key]);
+    //     }
+
+    //     return $accumulator;
+    // }
+}
